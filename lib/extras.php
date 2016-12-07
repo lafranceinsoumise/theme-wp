9c99b508b7ff3ba390217dc6022c6fa5ba55968f
Guillaume Royer <perso@guilro.com>
2016-12-07 19:31:24 +0100

    customize woocommerce checkout

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,30 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Continued', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+/**
+ * Change WooCommerce checkout page
+ */
+add_filter('woocommerce_enable_order_notes_field', '__return_false');
+add_filter('woocommerce_checkout_fields', __NAMESPACE__ . '\\woocommerce_override_checkout_fields');
+
+function woocommerce_override_checkout_fields($fields)
+{
+     $new_fields = [
+       'billing_first_name' => $fields['billing']['billing_first_name'],
+       'billing_last_name' => $fields['billing']['billing_last_name'],
+       'billing_address_1' => $fields['billing']['billing_address_1'],
+       'billing_postcode' => $fields['billing']['billing_postcode'],
+       'billing_city' => $fields['billing']['billing_city'],
+       'billing_email' => $fields['billing']['billing_email'],
+       'billing_phone' => $fields['billing']['billing_phone'],
+     ];
+
+    foreach ($new_fields as $key => $value) {
+        $new_fields[$key]['class'][0] = 'form-row-wide';
+    }
+
+    $fields['billing'] = $new_fields;
+
+    return $fields;
+}
