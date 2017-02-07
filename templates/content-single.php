da6504c180977704530685562cc74645333f74a3
Guillaume Royer <perso@guilro.com>
2017-02-07 17:22:04 +0100

    add caption

--- templates/content-single.php
+++ templates/content-single.php
@@ -3,6 +3,7 @@
     <header>
       <?php if (has_post_thumbnail() && get_post_format() !== 'video') {
         the_post_thumbnail('large');
+        echo '<p><em class="text-muted"><small>'.get_post(get_post_thumbnail_id())->post_excerpt.'</small></em></p>';
       } ?>
       <h1 class="entry-title"><?php the_title(); ?></h1>
       <?php get_template_part('templates/entry-meta'); ?>
