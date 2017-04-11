f336712bd4132eda3af0bdacaf8e34fa5c86cc86
flow <simon.florian.toulouse@gmail.com>
2017-04-11 14:46:09 +0200

    add comment feature in pages' templates

--- template-banner.php
+++ template-banner.php
@@ -6,4 +6,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('templates/content', 'banner'); ?>
+
+    <?php comments_template('/templates/comments.php'); ?>
 <?php endwhile; ?>
