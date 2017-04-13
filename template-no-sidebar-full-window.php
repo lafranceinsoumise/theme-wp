166db0df7c6f3380218cddbe21198d5a83a387a8
devichase <simon.florian.toulouse@gmail.com>
2017-04-13 18:10:31 +0200

    add comment feature in pages' templates (#19)

--- template-no-sidebar-full-window.php
+++ template-no-sidebar-full-window.php
@@ -7,4 +7,6 @@
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
+
+  <?php comments_template('/templates/comments.php'); ?>
 <?php endwhile; ?>
