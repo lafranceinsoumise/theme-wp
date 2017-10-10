a2933b8943c08574d9cf8a4d0154d282267b7e18
Guillaume Royer <perso@guilro.com>
2017-10-10 16:36:53 +0200

    hide page header if title starts with an antislash

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
 <?php use Roots\Sage\Titles; ?>
-<?php if(!is_front_page()) {?>
+<?php if(!is_front_page() && Titles\title()[0] !== '\\') {?>
 <div class="<?=(is_front_page())? '' : 'page-header'?>">
   <h1><?= Titles\title(); ?></h1>
   <?php
