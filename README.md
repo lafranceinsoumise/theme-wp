81d0dd9b662e011e370de972df82ff4099066dd4
Jill Royer <hello@jillroyer.me>
2020-01-16 17:12:53 +0100

    feat: primary navbar

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LFI Wordpress Theme
+
+## Development
+
+Load this theme on a Wordpress installation served on localhost:5000.
+
+Then launch `npm start` in the theme directory.
--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,10 @@
 export default {
   init() {
     // JavaScript to be fired on all pages
+    $('.menu-item-has-children').addClass('dropdown');
+    $('.sub-menu').addClass('dropdown-menu');
+    $('.sub-menu .menu-item').addClass('dropdown-item');
+    $('.menu-item-has-children > a').attr('data-toggle', 'dropdown').dropdown();
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
