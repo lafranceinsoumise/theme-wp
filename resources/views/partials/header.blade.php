3ece504952b3d52da29db459818538348d61a629
Jill Royer <hello@jillroyer.me>
2020-01-23 17:58:19 +0100

    feat: secondary navbar

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,19 @@
 <header class="banner">
   <div class="container">
-    <nav class="navbar navbar-light navbar-expand-lg">
-      <a class="navbar-brand mr-auto" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
+    <nav class="navbar navbar-primary">
+      <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav']) !!}
       @endif
     </nav>
   </div>
+
+  <nav class="navbar navbar-secondary">
+    <div class="container">
+      @if (has_nav_menu('secondary_navigation'))
+        {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'navbar-nav']) !!}
+      @endif
+      <?php get_search_form(); ?>
+    </div>
+  </nav>
 </header>
