4e9315629a62021cd81443fe8607f929e142ae87
Jill Royer <hello@jillroyer.me>
2020-01-24 15:49:09 +0100

    feat: breadcrumbs

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner">
   <div class="container">
-    <nav class="navbar navbar-primary">
+    <nav class="navbar navbar-primary" role="navigation">
       <a class="navbar-brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'navbar-nav']) !!}
@@ -8,7 +8,7 @@
     </nav>
   </div>
 
-  <nav class="navbar navbar-secondary">
+  <nav class="navbar navbar-secondary" role="navigation">
     <div class="container">
       @if (has_nav_menu('secondary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'secondary_navigation', 'menu_class' => 'navbar-nav']) !!}
@@ -16,4 +16,10 @@
       <?php get_search_form(); ?>
     </div>
   </nav>
+
+  @if (!is_front_page() && function_exists('yoast_breadcrumb'))
+    <nav aria-label="breadcrumb" class="container">
+      {!! yoast_breadcrumb('<ol class="breadcrumb">','</ol>') !!}
+    </nav>
+  @endif
 </header>
