04a10a50caa5ec984ab8fffc54ffc3d8db4d0d6f
Jill Royer <hello@jillroyer.me>
2020-05-06 15:58:03 +0200

    feat: reduce search form dimension

---
+++ searchform.php
@@ -0,0 +1,13 @@
+<form role="search" method="get" class="search-form">
+  <div class="input-group">
+    <label>
+      <span class="screen-reader-text">Rechercher&nbsp;:</span>
+      <input type="search" class="search-field" placeholder="Recherche…" value="" name="s" style="width: 200px">
+    </label>
+    <span class="input-group-btn">
+      <button type="submit" class="search-submit" style="line-height: 22px;">
+        <span class="glyphicon glyphicon-search"></span>
+      </button>
+    </span>
+  </div>
+</form>
